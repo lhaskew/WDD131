fdf3fd562a565d74d7a00a0daf297187cf8313ed
Lucy <has21013@byui.edu>
2025-01-18 16:03:31 -0700

    dark mode settings

--- mission/mission.js
+++ mission/mission.js
@@ -6,13 +6,25 @@ function selectTheme() {
 
     if (themeSelector.value === 'dark') {
         document.body.classList.add('dark');
+        document.body.classList.remove('light');
         document.querySelector('.logo').src = 'byui-logo_white.webp';
         console.log('Dark theme applied');
     } else {
         document.body.classList.remove('dark');
+        document.body.classList.add('light');
         document.querySelector('.logo').src = 'byui-logo_blue.webp';
         console.log('Light theme applied');
     }
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    if (themeSelector.value === 'dark') {
+        document.body.classList.add('dark');
+        document.querySelector('.logo').src = 'byui-logo_white.webp';
+    } else {
+        document.body.classList.add('light');
+        document.querySelector('.logo').src = 'byui-logo_blue.webp';
+    }
+});
 
 themeSelector.addEventListener('change', selectTheme);
